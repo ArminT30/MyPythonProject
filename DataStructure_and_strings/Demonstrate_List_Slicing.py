040ac28c36187d1e167e17644065bc96244a8853
ArminT30 <armintaranum1@gmail.com>
2025-07-29 15:51:42 +0530

    Committed assignment 4 and 5

---
+++ DataStructure_and_strings/Demonstrate_List_Slicing.py
@@ -0,0 +1,11 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print("Original list:", numbers)
+
+first_five = numbers[0:5]
+
+print("Extracted first five elements:", first_five)
+
+reversed_five = first_five[::-1]
+
+print("Reversed extracted elements:", reversed_five)
